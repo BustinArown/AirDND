c0acd885872afa04f4cce6b3efd360e7d78756ff
nathantfrank <nathan.t.frank@gmail.com>
2017-11-20 08:50:32 -0600

    added api v1.0.0 boilerplate

---
+++ controllers/admins_controller.py
@@ -0,0 +1,57 @@
+
+def add_listing(listing) -> str:
+    return 'do some magic!'
+
+def add_reservation(reservation) -> str:
+    return 'do some magic!'
+
+def add_review(review) -> str:
+    return 'do some magic!'
+
+def add_user(user) -> str:
+    return 'do some magic!'
+
+def get_listing(listingId) -> str:
+    return 'do some magic!'
+
+def get_reservation(reservationId) -> str:
+    return 'do some magic!'
+
+def get_review(reviewId) -> str:
+    return 'do some magic!'
+
+def get_reviews(listingId) -> str:
+    return 'do some magic!'
+
+def get_users(userId) -> str:
+    return 'do some magic!'
+
+def getsimilar_listings(listingId, startDate = None, endDate = None, sectionOffset = None, listingsOffset = None) -> str:
+    return 'do some magic!'
+
+def remove_listing(listingId) -> str:
+    return 'do some magic!'
+
+def remove_reservation(reservationId) -> str:
+    return 'do some magic!'
+
+def remove_review(reviewId) -> str:
+    return 'do some magic!'
+
+def remove_user(userId) -> str:
+    return 'do some magic!'
+
+def search_listing(location, startDate = None, endDate = None, petsAllowed = None, numGuests = None, numAdults = None, sectionOffset = None, listingsOffset = None) -> str:
+    return 'do some magic!'
+
+def update_listing(listingId, listing) -> str:
+    return 'do some magic!'
+
+def update_reservation(reservationId, reservation) -> str:
+    return 'do some magic!'
+
+def update_review(review) -> str:
+    return 'do some magic!'
+
+def update_user(userId, user) -> str:
+    return 'do some magic!'
